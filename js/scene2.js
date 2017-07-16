9f34e214dcbe19c6b756bf1a4de9d6d2864473c4
Ronen Annason <ronenannason83@gmail.com>
2017-07-16 14:17:21 -0400

    setting stage, replacing setting of last scene with the setting necessary for this scene which is a castle

---
+++ js/scene2.js
@@ -0,0 +1,39 @@
+function scene2() {
+  // testing to make sure our scene2.js file is wolrking ok.
+  console.log('Executing scene 2!');
+
+  // Setup Stage
+  stage =     document.querySelector('#stage')
+
+  // get source of the setting you want to implement - in this case castle
+  castleImgSrc = castle.getAttribute('src')
+  // reset stageImg src, with the image source of the setting you want to change it to.
+  stageImg.setAttribute('src', castleImgSrc)
+    
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
